4582da41f358289aea48f391f05a636a178c1f35
trnghieu <147842315+trnghieu@users.noreply.github.com>
2026-06-02 11:42:20 +0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173", env.clientUrl,
+    origin: ["http://localhost:5173", env.clientUrl],
     credentials: true
   })
 );
